ca44f6637f6750cad727f98915b9852e73e51f2a
Mert Ekdal <mertekdal@hotmail.com>
2023-09-09 11:59:40 +0300

    Reconfigured home page done state

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learningdart/firebase_options.dart';
 import 'package:learningdart/views/login_view.dart';
 import 'package:learningdart/views/register_view.dart';
+import 'package:learningdart/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,16 +33,17 @@ class HomePage extends StatelessWidget {
         builder: ((context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              return const LoginView();
-            // final user = FirebaseAuth.instance.currentUser;
-            // print(user);
-            // if (user == null) {
-            //   return const HomePage();
-            // } else if (user.emailVerified) {
-            //   return const Text('Verified');
-            // } else {
-            //   return const VerifyEmailView();
-            // }
+              final user = FirebaseAuth.instance.currentUser;
+              print(user);
+              if (user != null) {
+                if (user.emailVerified) {
+                } else {
+                  return const VerifyEmailView();
+                }
+              } else {
+                return const LoginView();
+              }
+              return const Text('Done');
             default:
               return const CircularProgressIndicator();
           }
